787a9f111867e9433492588b8137ba1e89303041
Brandon Marceau <54906026+BrandonMarceau@users.noreply.github.com>
2021-12-05 18:38:43 -0700

    Add files via upload

--- BluetoothCarControl/BluetoothCarControl.ino
+++ BluetoothCarControl/BluetoothCarControl.ino
@@ -130,8 +130,8 @@ void forwardSlow() {
     digitalWrite(MOTOR_2F, HIGH);   
     digitalWrite(MOTOR_1B, LOW);
     digitalWrite(MOTOR_2B, LOW);
-    analogWrite(MOTOR_1PWM, 50);
-    analogWrite(MOTOR_2PWM, 50);
+    analogWrite(MOTOR_1PWM, 125);
+    analogWrite(MOTOR_2PWM, 125);
 }
 
 //Function for moving medium speed forward
@@ -140,8 +140,8 @@ void forward() {
     digitalWrite(MOTOR_2F, HIGH);   
     digitalWrite(MOTOR_1B, LOW);
     digitalWrite(MOTOR_2B, LOW);
-    analogWrite(MOTOR_1PWM, 125);
-    analogWrite(MOTOR_2PWM, 125);
+    analogWrite(MOTOR_1PWM, 175);
+    analogWrite(MOTOR_2PWM, 175);
 }
 
 //Function for moving fast speed forward
@@ -150,8 +150,8 @@ void forwardFast() {
    digitalWrite(MOTOR_2F, HIGH);   
    digitalWrite(MOTOR_1B, LOW);
    digitalWrite(MOTOR_2B, LOW);
-   analogWrite(MOTOR_1PWM, 250);
-   analogWrite(MOTOR_2PWM, 250); 
+   analogWrite(MOTOR_1PWM, 255);
+   analogWrite(MOTOR_2PWM, 255); 
 }
 
 //Function for backwards movement
@@ -160,8 +160,8 @@ void backward() {
     digitalWrite(MOTOR_2F, LOW);   
     digitalWrite(MOTOR_1B, HIGH);
     digitalWrite(MOTOR_2B, HIGH);
-    analogWrite(MOTOR_1PWM, 125);
-    analogWrite(MOTOR_2PWM, 125);  
+    analogWrite(MOTOR_1PWM, 150);
+    analogWrite(MOTOR_2PWM, 150);  
 }
 
 //Turning left, sets right motor forward and left motor backward for track like turning
@@ -170,8 +170,8 @@ void turnLeft() {
   digitalWrite(MOTOR_1B, HIGH);
   digitalWrite(MOTOR_2B, LOW);
   digitalWrite(MOTOR_2F, HIGH);
-  analogWrite(MOTOR_1PWM, 100);
-  analogWrite(MOTOR_2PWM, 125);
+  analogWrite(MOTOR_1PWM, 150);
+  analogWrite(MOTOR_2PWM, 250);
 }
 
 //Turning left, sets left motor forward and right motor backward for track like turning
@@ -180,8 +180,8 @@ void turnRight() {
   digitalWrite(MOTOR_2B, HIGH);
   digitalWrite(MOTOR_1B, LOW);
   digitalWrite(MOTOR_1F, HIGH);
-  analogWrite(MOTOR_1PWM, 125);
-  analogWrite(MOTOR_2PWM, 100);
+  analogWrite(MOTOR_1PWM, 250);
+  analogWrite(MOTOR_2PWM, 150);
 }
 
 //Function to stop car, turns all motors off
